147bb09bdf51af7c9f444a6705366e2a8b04ac6c
Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>
2022-11-29 12:54:26 -0300

    fix the BlockEvent deserialization (#17)
    * fix the BlockEvent deserialization
    
    because ser and deser implementations are not inverses right now
    
    * use Vec<u8> and derive instead of SerializedBlock

--- blockchain-source/src/cardano/event.rs
+++ blockchain-source/src/cardano/event.rs
@@ -1,8 +1,6 @@
 use crate::{EventObject, GetNextFrom};
 use anyhow::Context;
-use cardano_sdk::protocol::SerializedBlock;
 use dcspark_core::{BlockId, BlockNumber, SlotNumber};
-use serde::de::Visitor;
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub enum CardanoNetworkEvent<Block, Tip> {
@@ -17,12 +15,12 @@ impl<Block: Send, Tip: Send> EventObject for CardanoNetworkEvent<Block, Tip> {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 pub struct BlockEvent {
     pub id: BlockId,
     pub parent_id: BlockId,
     pub block_number: BlockNumber,
-    pub raw_block: SerializedBlock,
+    pub raw_block: Vec<u8>,
     pub slot_number: SlotNumber,
     pub is_boundary_block: bool,
     pub epoch: Option<u64>,
@@ -47,48 +45,6 @@ impl<Block, Tip> CardanoNetworkEvent<Block, Tip> {
             CardanoNetworkEvent::Block(block) => Ok(CardanoNetworkEvent::Block(block)),
             CardanoNetworkEvent::Tip(tip) => f(tip).map(CardanoNetworkEvent::Tip),
         }
-    }
-}
-
-impl serde::Serialize for BlockEvent {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_bytes(self.raw_block.as_ref())
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for BlockEvent {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        struct V;
-
-        impl<'de> Visitor<'de> for V {
-            type Value = BlockEvent;
-
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                formatter.write_str("a byte array")
-            }
-
-            fn visit_bytes<E>(self, buf: &[u8]) -> Result<Self::Value, E>
-            where
-                E: serde::de::Error,
-            {
-                let mut reader = cbored::Reader::new(buf);
-
-                let raw_block: SerializedBlock = reader
-                    .decode()
-                    .context("invalid block")
-                    .map_err(serde::de::Error::custom)?;
-
-                BlockEvent::from_serialized_block(raw_block).map_err(serde::de::Error::custom)
-            }
-        }
-
-        deserializer.deserialize_bytes(V)
     }
 }
 
@@ -147,10 +103,9 @@ impl<Tip> GetNextFrom for CardanoNetworkEvent<BlockEvent, Tip> {
 }
 
 impl BlockEvent {
-    pub(crate) fn from_serialized_block(raw_block: SerializedBlock) -> anyhow::Result<Self> {
-        let block = raw_block
-            .unserialize()
-            .context("failed to deserialize block");
+    pub(crate) fn from_serialized_block(raw_block: &[u8]) -> anyhow::Result<Self> {
+        let block: anyhow::Result<cardano_sdk::chain::Block> =
+            cbored::decode_from_bytes(raw_block).context("failed to deserialize block");
 
         if let Ok(block) = block {
             let header = block.header();
@@ -160,7 +115,7 @@ impl BlockEvent {
             let parent_id = get_parent_id(&block.header());
 
             Ok(BlockEvent {
-                raw_block,
+                raw_block: raw_block.to_vec(),
                 id,
                 parent_id,
                 block_number,
@@ -172,10 +127,10 @@ impl BlockEvent {
                 // it can be computed later inside carp, since we don't need this in the bridge.
                 epoch: None,
             })
-        } else if let Ok(block) = crate::cardano::byron::ByronBlock::decode(raw_block.as_ref()) {
+        } else if let Ok(block) = crate::cardano::byron::ByronBlock::decode(raw_block) {
             let header = block.header();
             let event = BlockEvent {
-                raw_block,
+                raw_block: raw_block.to_vec(),
                 id: BlockId::new(block.hash().to_string()),
                 parent_id: BlockId::new(header.previous_hash().to_string()),
                 block_number: header.block_number(),
@@ -187,7 +142,7 @@ impl BlockEvent {
             Ok(event)
         } else {
             tracing::error!(
-                block = hex::encode(raw_block.as_ref()),
+                block = hex::encode(raw_block),
                 "failed to deserialize block"
             );
             block.map(|_| unreachable! {})
